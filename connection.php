42e7f1815e71fda870d9ebf674ad50d84d6ce324
Rawa Jalal <rawa.jalal@gmail.com>
2017-08-02 14:40:22 -0400

    update connection string for ms access driver

--- connection.php
+++ connection.php
@@ -3,11 +3,10 @@
 	function ConnectToDatabase()
 	{
 
-		$connectionString = 'odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=C:\\xampp\\htdocs\\Assignment5\\Data\\assignment5.mdb';
+		$connectionString = 'odbc:Driver={Microsoft Access Driver (*.mdb)};Dbq=C:\\xampp\\htdocs\\Assignment5\\Data\\assignment5.mdb';
 
 		$connection = new PDO($connectionString);
 		$connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 		return $connection;
 
 	}
