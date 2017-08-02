6073918ca219992d9385bfb4e7e29b1c69718326
Rawa Jalal <rawa.jalal@gmail.com>
2017-08-02 19:01:12 -0400

    finished part form html and css

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
 
 	/*
 	* Creates a <select> dropdown list, taking a SELECT query ($theQuery) as the first parameter,
-	* the <option> value as $valueName, the <option> description as $descriptionName
+	* the <option> value as $valueID, the <option> description as $valueDescription
 	* Returns a concatenated output of all of the options
 	*/
-	function DropdownListFor($theQuery,$valueName,$descriptionName){
+	function DropdownListFor($theQuery,$valueID,$valueDescription){
 		$output = "";
 		while($row = $theQuery ->fetch()){
-			$output .='<option value="' . $row[$valueName] . '">' . $row[$descriptionName] . '</option>';
+			$output .='<option value="' . $row[$valueID] . '">' . $row[$valueDescription] . '</option>';
 		}
 		return $output;
 	}
@@ -36,29 +36,59 @@
 </div>
 <div class="container">
 	
-	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST" role="form">
-		<legend>Form title</legend>
-		
-		<div class="form-group">
-			<label for="VendorNo">Vendor</label>
+	<div class="well">
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST" role="form">
+			<legend><h2>Insert record into the Parts table</h2></legend>
 			
-			<select name="VendorNo" id="VendorNo" class="form-control" required="required">
-				<option value="">Select a vendor</option>
-				<?php
-					echo DropdownListFor($vendorsQuery,"VendorNo","VendorName");	
-				?>
-			</select>
-		</div>
-		<div class="form-group">
-			<label for="">label</label>
-			<input type="text" class="form-control" id="" placeholder="Input field">
-		</div>
-	
-		
-	
-		<button type="submit" class="btn btn-primary">Submit</button>
-	</form>
-	
+			<div class="form-group row">
+				<label class="col-md-2" for="VendorNo">Vendor</label>
+				<div class="col-md-10">
+					<select name="VendorNo" id="VendorNo" class="form-control" required="required">
+						<option value="">Select a vendor</option>
+						<?php
+							echo DropdownListFor($vendorsQuery,"VendorNo","VendorName");	
+						?>
+					</select>
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-md-2" for="Description">Part Description</label>
+				<div class="col-md-10">
+					<input type="text" class="form-control" id="Description" name="Description">
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-md-2" for="">Parts on hand</label>
+				<div class="col-md-10">
+					<input type="text" class="form-control" id="OnHand" name="OnHand" placeholder="Enter positive integers only">
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-md-2" for="">Parts on order</label>
+				<div class="col-md-10">
+					<input type="text" class="form-control" id="OnOrder" name="OnOrder" placeholder="Enter positive integers only">
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-md-2" for="">Cost</label>
+				<div class="col-md-10">
+					<input type="text" class="form-control" id="Cost" name="Cost">
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-md-2" for="">List Price</label>
+				<div class="col-md-10">
+					<input type="text" class="form-control" id="ListPrice" name="ListPrice">
+				</div>
+			</div>
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</form>
+	</div>
 </div>
 
 <?php
