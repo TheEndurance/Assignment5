50eae2ae35f12242bd5aca0121f8f0b06e8fd68e
Rawa Jalal <rawa.jalal@gmail.com>
2017-08-06 19:51:58 -0400

    form validation and sticky post values

--- parts.php
+++ parts.php
@@ -1,12 +1,165 @@
+
 <?php
-    include('includes/header.html');
+	session_start();
+
+	include('includes/header.html');
+	require('connection.php');
+	require('requires/post-validation.php');
+
+	//error array
+	$errors = array();
+
+
+
+	//Database Connection
+	$connection = ConnectToDatabase();
+
+	//Vendor Number and Name query
+	$vendorsQuery = $connection->prepare("SELECT VendorNo,VendorName from Vendors");
+	$vendorsQuery -> execute();
+
 	
-  
+	/*
+	* Creates a <select> dropdown list, taking a SELECT query ($theQuery) as the first parameter,
+	* the <option> value as $valueID, the <option> description as $valueDescription
+	* echoes a concatenated output of all of the options
+	*/
+	function DropdownListFor($theQuery,$valueID,$valueDescription){
+		$output = "";
+		while($row = $theQuery ->fetch()){
+			if (StickyForm($valueID)!=null && StickyForm($valueID)==$row[$valueID]){				
+				$output .='<option value="' . $row[$valueID] .'" SELECTED>' . $row[$valueDescription] . '</option>';	
+			} else {
+				$output .='<option value="' . $row[$valueID] .'">' . $row[$valueDescription] . '</option>';
+			}
+		}
+		echo $output;
+	}
 
-    	
+	function ValidationSummaryFor($postName){
+		global $errors;
+		if (!empty($errors[$postName])){
+			echo '<p class="error">' . $errors[$postName] . '</p>';
+		}
+	}
+	
+	//When the form is submitted
+	if ($_SERVER['REQUEST_METHOD']=='POST'){
+        $_SESSION['VendorNo'] = ValidatePost("VendorNo","Vendor",$partDataValidation,$partValidationMessage,false,"A vendor must be selected");
+        $_SESSION['Description'] = ValidatePost("Description","Part description",$partDataValidation,$partValidationMessage);
+        $_SESSION['OnHand'] = ValidatePost("OnHand","Parts on hand",$partDataValidation,$partValidationMessage);
+        $_SESSION['OnOrder'] = ValidatePost("OnOrder","Parts on order",$partDataValidation,$partValidationMessage);
+        $_SESSION['Cost'] = ValidatePost("Cost","Cost",$partDataValidation,$partValidationMessage);
+        $_SESSION['ListPrice'] = ValidatePost("ListPrice","List price",$partDataValidation,$partValidationMessage);
+    } // END IF POST
 ?>
 
+<div class="jumbotron">
+	<div class="container">
+		<h1>Hello, world!</h1>
+		<p>Contents ...</p>
+		<p>
+			<a class="btn btn-primary btn-lg">Learn more</a>
+		</p>
+	</div>
+</div>
+<div class="container">
+	<div class="well">
+		<?php
+		if(count($errors)>0){
+		?>
+			<div class="errors">
+				<div class="alert alert-dismissible alert-danger">
+					<button type="button" class="close" data-dismiss="alert">&times;</button>
+					<strong>Oh snap!</strong> <a href="#" class="alert-link">Change a few things up</a> and try submitting again.
+				</div>
+				<ul>
+					<?php
+						foreach($errors as $error){
+							echo '<li class="error">'.$error.'</li>';
+						}
+					?>
+				</ul>
+			</div>
+		<?php
+		}
+		?>
+		<form action="index.php" method="POST" role="form">
+			<legend><h2>Insert record into the Parts table</h2></legend>
+			
+			<div class="form-group row">
+				<label class="col-md-2" for="VendorNo">Vendor</label>
+				<div class="col-md-10">
+					<select name="VendorNo" id="VendorNo" class="form-control" >
+						<option value="null">Select a vendor</option>
+						<?php
+							DropdownListFor($vendorsQuery,"VendorNo","VendorName");	
+						?>
+					</select>
+					<?php
+					ValidationSummaryFor("VendorNo");
+					?>
+				</div>
+				
+			</div>
+
+			<div class="form-group row">
+				<label class="col-md-2" for="Description">Part Description</label>
+				<div class="col-md-10">
+					<input type="text" class="form-control" id="Description" name="Description"  value="<?php echo StickyForm('Description') ?>">
+					<?php
+					ValidationSummaryFor("Description");
+					?>
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-md-2" for="">Parts on hand</label>
+				<div class="col-md-10">
+					<input type="text" class="form-control" id="OnHand" name="OnHand" placeholder="Enter positive integers only" value="<?php echo StickyForm('OnHand') ?>">
+					<?php
+					ValidationSummaryFor("OnHand");
+					?>
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-md-2" for="">Parts on order</label>
+				<div class="col-md-10">
+					<input type="text" class="form-control" id="OnOrder" name="OnOrder" placeholder="Enter positive integers only" value="<?php echo StickyForm('OnOrder') ?>">
+					<?php
+					ValidationSummaryFor("OnOrder");
+					?>
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-md-2" for="">Cost</label>
+				<div class="col-md-10">
+					<input type="text" class="form-control" id="Cost" name="Cost" value="<?php echo StickyForm('Cost') ?>">
+					<?php
+					ValidationSummaryFor("Cost");
+					?>
+				</div>
+			</div>
+
+			<div class="form-group row">
+				<label class="col-md-2" for="">List Price</label>
+				<div class="col-md-10">
+					<input type="text" class="form-control" id="ListPrice" name="ListPrice" value="<?php echo StickyForm('ListPrice') ?>">
+					<?php
+					ValidationSummaryFor("ListPrice");
+					?>
+				</div>
+			</div>
+			<button type="submit" name="submit" class="btn btn-primary">Submit</button>
+		</form>
+	</div>
+</div>
 
 <?php
-    include('includes/footer.html');
+	include('includes/footer.html');	
 ?>
+
+
+
--- requires/post-validation.php
+++ requires/post-validation.php
@@ -14,34 +14,32 @@ $vendorDataTypes = array(
     "Fax" => "string"
 );
 //Parts Table Data Types
-$partDataTypes = array(
-    "VendorNo" => "number",
-    "Description" => "string",
-    "OnHand" => "number",
-    "OnOrder" => "number",
-    "Cost" => "number",
-    "ListPrice" => "number"
+$partValidationMessage = array(
+    "VendorNo" => "A Vendor must be selected",
+    "Description" => "Description must contain atleast one character",
+    "OnHand" => "Parts on hand can only be a numeric value",
+    "OnOrder" => "Parts on order can only be a numeric value",
+    "Cost" => "Cost can only be a numeric value",
+    "ListPrice" => "List price can only be a numeric value"
 );
 
+$partDataValidation = array(
+    "VendorNo" => "/[0-9]+/",
+    "Description" => "/.+/",
+    "OnHand" => "/[0-9]+/",
+    "OnOrder" => "/[0-9]+/",
+    "Cost" => "/[0-9]+/",
+    "ListPrice" => "/[0-9]+/"
+);
 /*
 * Helper function for ValidatePost, that returns true if the value is the specified data type.
 */
-function ValidateDataType($value, $dataType)
+function ValidateRegex($value, $regex)
 {
-    switch ($dataType) {
-        case "number":
-            if (is_numeric($value)) {
-                return true;
-            }
-            break;
-        case "string":
-            if (is_string($value)) {
-                return true;
-            }
-            break;
-        default:
-            return false;
+    if(preg_match($regex,$value)==1){
+        return true;
     }
+    return false;
 }
 
 /*
@@ -49,24 +47,35 @@ function ValidateDataType($value, $dataType)
 * the $errors array if it is empty and returns null.  
 * Otherwise it will return the POST value.
 */
-function ValidatePost($postName, $displayName, $tableDataTypes,$allowNull = false, $customMessage = "")
+function ValidatePost($postName, $displayName, $tableDataValidation,$tableValidationMessage,$allowNull = false, $customMessage = "")
 {
     global $errors;
     
     if (empty($_POST[$postName])) { //generic field is required message
         if ($allowNull==false) {
-            $errors[$postName]= $displayName . ' is a required field.';
+            $errors[$postName]= $displayName . ' is a required field';
         }
         return null;
-    } elseif (!ValidateDataType($_POST[$postName], $tableDataTypes[$postName])) { //POST value is not the correct data type
+    } elseif (!ValidateRegex($_POST[$postName], $tableDataValidation[$postName])) { //POST value is not the correct data type
         if (strlen($customMessage)>0) { // Check if a custom error message has been specified
             $errors[$postName] = $customMessage;
             return null;
         } else { // else use the generic data type error message
-            $errors[$postName] = $displayName . ' data type must be a ' . $tableDataTypes[$postName] . '.';
+            $errors[$postName] = $tableValidationMessage[$postName];
             return null;
         }
     } else {
         return addslashes($_POST[$postName]); //return the popst value
+    }
+}
+
+/*
+* Sets the value of an input field in a form if the page has been posted and the value is set
+*/
+function StickyForm($postVariable){
+    if(isset($_POST[$postVariable])){
+        return $_POST[$postVariable];
+    } else {
+        return null;
     }
 }
